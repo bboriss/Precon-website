0ec76068a00957f094c84a323f14aa0abccdb708
bboriss <borisstevanovic0810@gmail.com>
2025-12-23 19:05:44 +0100

    23-12-2025

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="sr">
-      <body>{children}</body>
+    <html lang="sr" suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
